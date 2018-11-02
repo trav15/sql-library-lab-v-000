8358c4f7642c536666fa6d4a6b5cf04a4b519691
Travis T <travistokuyama@gmail.com>
2018-11-02 14:38:45 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -21,3 +21,18 @@ CREATE TABLE books (
   year INTEGER
   series_id INTEGER
 );
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+  name TEXT
+  motto TEXT
+  species TEXT
+  author_id INTEGER
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY
+  character_id INTEGER
+  book_id INTEGER 
+);
