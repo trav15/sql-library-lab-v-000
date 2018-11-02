a8a17b5eb9b38596e9da09a825685d57be6defa4
Travis T <travistokuyama@gmail.com>
2018-11-02 15:07:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  " SELECT series.title 
+  " SELECT series.title
     FROM series
     JOIN books
     ON books.series_id = series.id
@@ -31,5 +31,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  " SELECT characters.name, COUNT(*) as book_count 
+    FROM character_books
+    JOIN characters
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY book_count DESC"
 end
