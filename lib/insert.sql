d201f710cc11a0d7c9643a8e6a155764f52ea4b6
Travis T <travistokuyama@gmail.com>
2018-11-02 14:45:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,4 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'Game of Thrones', 1, 1), (2, 'Red Rising', 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, 'Song of Ice & Fire', 2000, 1), (2, 'Second Book', 2001, 1),
+(3, 'Third Book', 2002, 1), (4, 'Red Rising Book 1', 2010, 2), (5, 'Gold Something', 2011, 2), (6, 'Red Dead Redemption', 2012, 2);
