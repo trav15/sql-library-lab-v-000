4ca0e1def28307c9e6d27cbf429a7dce76f554de
Travis T <travistokuyama@gmail.com>
2018-11-02 14:36:07 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,5 +2,22 @@ CREATE TABLE series (
   id INTEGER PRIMARY key
   title TEXT
   author_id INTEGER
-  subgenre_id INTEGER  
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenre (
+  id INTEGER PRIMARY KEY
+  name TEXT
+);
+
+CREATE TABLE author (
+  id INTEGER PRIMARY KEY
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY
+  title TEXT
+  year INTEGER
+  series_id INTEGER
 );
